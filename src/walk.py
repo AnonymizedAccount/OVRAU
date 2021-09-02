039ef25284e1422741dfdd7671e7b41e81b0f6b8
Anonymized Account <amaraamina2290@gmail.com>
2021-09-02 10:29:11 +0100

    Add files via upload

---
+++ src/walk.py
@@ -0,0 +1,118 @@
+import itertools
+import random
+import networkx as nx
+import numpy as np
+from joblib import Parallel, delayed
+
+def preprocess_nxgraph(graph):
+    node2idx = {}
+    idx2node = []
+    node_size = 0
+    for node in graph.nodes():
+        node2idx[node] = node_size
+        idx2node.append(node)
+        node_size += 1
+    return idx2node, node2idx
+
+def partition_dict(vertices, workers):
+    batch_size = (len(vertices) - 1) // workers + 1
+    part_list = []
+    part = []
+    count = 0
+    for v1, nbs in vertices.items():
+        part.append((v1, nbs))
+        count += 1
+        if count % batch_size == 0:
+            part_list.append(part)
+            part = []
+    if len(part) > 0:
+        part_list.append(part)
+    return part_list
+
+
+def partition_list(vertices, workers):
+    batch_size = (len(vertices) - 1) // workers + 1
+    part_list = []
+    part = []
+    count = 0
+    for v1, nbs in enumerate(vertices):
+        part.append((v1, nbs))
+        count += 1
+        if count % batch_size == 0:
+            part_list.append(part)
+            part = []
+    if len(part) > 0:
+        part_list.append(part)
+    return part_list
+	
+def partition_num(num, workers):
+    if num % workers == 0:
+        return [num//workers]*workers
+    else:
+        return [num//workers]*workers + [num % workers]
+
+class RWGraph():
+    def __init__(self, nx_G, node_type=None):
+        self.G = nx_G
+        self.node_type = node_type
+    def deepwalk_walk(self, walk_length, start_node):
+
+        walk = [start_node]
+
+        while len(walk) < walk_length:
+            cur = walk[-1]
+            cur_nbrs = list(self.G.neighbors(cur))
+            if len(cur_nbrs) > 0:
+                walk.append(random.choice(cur_nbrs))
+            else:
+                break
+        return walk
+
+    def node2vec_walk(self, walk_length, start_node):
+
+        G = self.G
+        alias_nodes = self.alias_nodes
+        alias_edges = self.alias_edges
+
+        walk = [start_node]
+
+        while len(walk) < walk_length:
+            cur = walk[-1]
+            cur_nbrs = list(G.neighbors(cur))
+            if len(cur_nbrs) > 0:
+                if len(walk) == 1:
+                    walk.append(
+                        cur_nbrs[alias_sample(alias_nodes[cur][0], alias_nodes[cur][1])])
+                else:
+                    prev = walk[-2]
+                    edge = (prev, cur)
+                    next_node = cur_nbrs[alias_sample(alias_edges[edge][0],
+                                                      alias_edges[edge][1])]
+                    walk.append(next_node)
+            else:
+                break
+
+        return walk
+
+    def simulate_walks(self, num_walks, walk_length, workers, verbose=0):
+
+        G = self.G
+
+        nodes = list(G.nodes())
+
+        results = Parallel(n_jobs=workers, verbose=verbose, )(
+            delayed(self._simulate_walks)(nodes, num, walk_length) for num in
+            partition_num(num_walks, workers))
+
+        walks = list(itertools.chain(*results))
+
+        return walks
+
+    def _simulate_walks(self, nodes, num_walks, walk_length,):
+        walks = []
+        for _ in range(num_walks):
+            random.shuffle(nodes)
+            for v in nodes:
+                walks.append(self.deepwalk_walk(walk_length=walk_length, start_node=v))
+                
+        return walks
